f1013fe8cbcfaa2fd421174f23af48c1002c6604
Chicha Ilyes <chicha.ilyes.business@gmail.com>
2023-01-14 17:46:37 +0100

    .

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -17,7 +17,7 @@ class _HomeState extends State<Home> {
     ParkingProvider p = Provider.of<ParkingProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Park"),
+        title: const Text("Park Me"),
         centerTitle: true,
       ),
       body: StreamBuilder(
